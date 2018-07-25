d51b0e71179a5c215c7cfa1b36d16764568d51b6
Approximator <alex@nls.la>
2018-07-25 21:44:27 +0200

    Added possibility to delete photos and determine missing photos.

--- apms/server/server.py
+++ apms/server/server.py
@@ -18,10 +18,14 @@ limitations under the License.
 
 import os
 import json
+import shutil
 import logging
+import datetime
 
-from tornado.ioloop import IOLoop
+import tornado
 from tornado_sqlalchemy import make_session_factory, SessionMixin
+from sqlalchemy.orm import joinedload
+from sqlalchemy import or_, and_, not_
 from tornado.web import RequestHandler, Application, StaticFileHandler
 
 from apms.lib.db.database import Photo, User, Album
@@ -35,24 +39,81 @@ class PhotosHandler(RequestHandler, SessionMixin):
 
     def get(self, *args, **kwargs):
         page = int(self.get_query_argument('page', 1))
-        limit = int(self.get_query_argument('limit', 50))
+        limit = int(self.get_query_argument('limit', 200))
         offset = (page - 1) * limit
         owner_id = self.get_query_argument('owner_id', None)
         sort_by = self.get_query_argument('sort_by', None)
         missing = self.get_query_argument('missing', None)
         to_delete = self.get_query_argument('to_delete', None)
         foreign = self.get_query_argument('foreign', None)
+        small = self.get_query_argument('small', None)
 
         with self.make_session() as session:
-            query = session.query(Photo).filter_by(deleted_by_me=(to_delete is not None))
-            if owner_id:
-                query = query.filter_by(owner_id=owner_id)
+            if to_delete is not None:
+                count, result = self.get_photos_to_delete(session)
+            elif missing is not None:
+                count, result = self.get_missing_photos(session)
+            else:
+                query = session.query(Photo).options(joinedload(Photo.owner)).filter(not_(Photo.deleted_by_me))
+                if owner_id:
+                    query = query.filter_by(owner_id=owner_id)
+                if small:
+                    query = query.filter(Photo.width < 450)
 
-            count = query.count()
-            result = query.order_by(Photo.date_downloaded.desc()).offset(offset).limit(limit).all()
+                count = query.count()
+                result = query.order_by(Photo.date_added.desc()).offset(offset).limit(limit).all()
+
             photos = {'count': count, 'photos': list(map(lambda photo: photo.to_json(), result))}
             self.set_header('Content-Type', 'application/json')
             self.write(json.dumps(photos))
+
+    def get_missing_photos(self, session):
+        query = session.query(Photo).options(joinedload(Photo.owner)).filter_by(deleted_by_me=False)
+        result = list(
+            filter(lambda photo: not os.path.exists(os.path.join(config.photos_dir, photo.dir_name, photo.file_name)),
+                   query.order_by(Photo.date_added.desc()).all()))
+        return len(result), result[0:200]
+
+    def get_photos_to_delete(self, session):
+        query = session.query(Photo).options(joinedload(Photo.owner)).filter_by(deleted_by_me=True)
+        result = list(
+            filter(lambda photo: os.path.exists(os.path.join(config.photos_dir, photo.dir_name, photo.file_name)),
+                   query.order_by(Photo.date_downloaded.desc()).all()))
+        return len(result), result
+
+    def delete(self, *args, **kwargs):
+        try:
+            photos_to_delete = json.loads(self.request.body.decode())['photos']
+        except Exception as ex:
+            log.info(ex)
+            self.set_status(400)
+            self.write(json.dumps({'result': 'Error', 'cause': 'Wrong request', 'description': str(ex)}))
+            return
+
+        log.info('Going to remove {}'.format(photos_to_delete))
+        with self.make_session() as session:
+            query = session.query(Photo).filter(Photo.id.in_(photos_to_delete))
+            count = query.count()
+            if not count:
+                self.set_status(404)
+                self.write(json.dumps({'result': 'Error', 'cause': 'Not Found'}))
+                return
+            for photo in query.all():
+                if not os.path.isdir(config.trash_dir):
+                    os.makedirs(config.trash_dir)
+
+                photo.deleted_by_me = True
+                fname = os.path.join(config.photos_dir, photo.dir_name, photo.file_name)
+                new_fname = os.path.join(config.trash_dir, photo.file_name)
+                log.info('Moving {} -> {}'.format(fname, new_fname))
+                try:
+                    shutil.move(fname, new_fname)
+                except OSError:
+                    log.exception('Cannot move {} -> {}'.format(fname, new_fname))
+                log.info('{} marked as deleted by me'.format(photo.id))
+            session.commit()
+
+            self.write(json.dumps({'result': 'Ok', 'deleted': photos_to_delete}))
 
 
 class MainHandler(RequestHandler):
@@ -90,18 +151,6 @@ class ApmsServer:
 
     def __init__(self):
         self._session_factory = make_session_factory(config.db_connection_string)
-        self._app = Application(
-            [
-                (r"/api/photos", PhotosHandler),
-                (r'/files/photos/(.*)', NonCachedStaticFileHandler, {
-                    'path': config.photos_dir
-                }),
-                (r'/', MainHandler),
-                (r'/(.*)', NonCachedStaticFileHandler, {
-                    'path': config.static_dir
-                }),
-            ],
-            session_factory=self._session_factory).listen(7777)
         self._app = Application([
             (r"/api/photos", PhotosHandler),
             (r"/api/users", UsersHandler),
@@ -118,4 +167,4 @@ class ApmsServer:
                                 session_factory=self._session_factory).listen(7777, '127.0.0.1')
 
     def run(self):
-        IOLoop.current().start()
+        tornado.ioloop.IOLoop.current().start()
