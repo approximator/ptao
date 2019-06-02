8c3921fdebd55e91786c1f227a50b0050afb9b8f
Approximator <alex@nls.la>
2019-06-02 22:01:19 +0200

    Improve "people" page

--- apms-ui/src/people/index.js
+++ apms-ui/src/people/index.js
@@ -1,42 +1,56 @@
 import React, { Component } from 'react';
-import { Button, Label, Icon, Table, Rating, Checkbox } from 'semantic-ui-react'
+import { NavLink } from 'react-router-dom';
+import { Icon, Table, Rating, Checkbox } from 'semantic-ui-react';
 import Moment from 'react-moment';
+import { connect } from 'react-redux';
+import { fetchUsers, pauseUpdate, userInfoOpen } from '../actions/usersAction';
+import { makeSearchParams } from '../utils/urlParams';
 import UserInfo from './user-info';
 
 class PeoplePage extends Component {
-
-    state = {
-        peolpe: []
-    }
-
-    constructor(props) {
-        super(props)
-    }
-
-    componentDidMount() {
-        fetch('/api/users')
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                this.setState({ peolpe: data['users'] });
-            })
+    componentWillMount() {
+        console.log('componentWillMount. Props', this.props);
+        this.props.fetchUsers();
     }
 
     render() {
-        const rows = this.state.peolpe.map((user) =>
+        const rows = this.props.users.map(user => (
             <Table.Row key={user.id}>
                 <Table.Cell>
-                    <Label as='a' style={{ pointerEvents: "auto" }} onClick={() => {
-                        let params = new URLSearchParams(this.props.location.search);
-                        params.set('owner_id', user.id);
-                        params.set('page', 1);
-                        this.props.history.push(`/photos?${params.toString()}`);
-                    }}><Icon name='user' />
-                    </Label>
+                    <UserInfo />
+                    <Icon name="user" onClick={() => this.props.userInfoOpen(user.id)} />
+                    <NavLink
+                        to={{
+                            pathname: '/photos',
+                            search: `${makeSearchParams({ owner_id: user.id, page: 1 }, this.props.location.search)}`
+                        }}
+                    >
+                        <Icon name="rss" />
+                    </NavLink>
+
+                    <NavLink
+                        to={{
+                            pathname: '/photos',
+                            search: `${makeSearchParams({ photos_by: user.id, page: 1 }, this.props.location.search)}`
+                        }}
+                    >
+                        <Icon name="copyright" />
+                    </NavLink>
+
+                    <NavLink
+                        to={{
+                            pathname: '/photos',
+                            search: `${makeSearchParams({ photos_of: user.id, page: 1 }, this.props.location.search)}`
+                        }}
+                    >
+                        <Icon name="images" />
+                    </NavLink>
                 </Table.Cell>
+
                 <Table.Cell>{`${user.first_name} ${user.last_name}`}</Table.Cell>
+
                 <Table.Cell>
-                    <Rating icon='star' defaultRating={0} maxRating={10} />
+                    <Rating icon="star" defaultRating={0} maxRating={10} />
                 </Table.Cell>
                 <Table.Cell>
                     <Moment unix fromNow>
@@ -44,34 +58,40 @@ class PeoplePage extends Component {
                     </Moment>
                 </Table.Cell>
                 <Table.Cell>
-                    <Checkbox toggle checked={user.pause_update}
+                    <Checkbox
+                        toggle
+                        checked={user.pause_update}
                         onChange={() => {
-                            // user.pause_update = !user.pause_update
-                            console.log(`Use ${user.id} pause_update ${!user.pause_update} -> ${user.pause_update}`)
+                            this.props.pauseUpdate(user.id, !user.pause_update);
                         }}
                     />
                 </Table.Cell>
             </Table.Row>
-        )
+        ));
         return (
             <div>
                 <Table compact celled>
                     <Table.Header>
                         <Table.Row>
-                            <Table.HeaderCell></Table.HeaderCell>
+                            <Table.HeaderCell />
                             <Table.HeaderCell>Name</Table.HeaderCell>
                             <Table.HeaderCell>Rating</Table.HeaderCell>
                             <Table.HeaderCell>Photos updated</Table.HeaderCell>
                             <Table.HeaderCell>Pause update</Table.HeaderCell>
                         </Table.Row>
                     </Table.Header>
-                    <Table.Body>
-                        {rows}
-                    </Table.Body>
+                    <Table.Body>{rows}</Table.Body>
                 </Table>
             </div>
         );
     }
 }
 
-export default PeoplePage;
+function mapStateToProps(state) {
+    return { users: state.userReducer.users };
+}
+
+export default connect(
+    mapStateToProps,
+    { fetchUsers, pauseUpdate, userInfoOpen }
+)(PeoplePage);
