33ffb3e8ba415fa5ba01f55135d06ec23b24a8ec
Approximator <alex@nls.la>
2019-11-14 21:44:23 +0100

    Multiple DB sessions test

--- apms/tests/test_db.py
+++ apms/tests/test_db.py
@@ -5,31 +5,23 @@ Database test
 # pylint: disable=all
 import json
 import asyncio
-import datetime
 import logging
+import concurrent
 from pprint import pprint
 
-from pytest import fixture
+import pytest
 
-from faker import Faker
+from apms.lib.db.database import Tag, User, Photo, Album, BASE
 
-from tornado_sqlalchemy import make_session_factory, SessionMixin
-from ..lib.db.database import Tag, User, Photo, Album, BASE
+from apms.lib.providers.pd.pdmanager import PDManager
+from apms.lib.config import config
 
-from sqlalchemy import Column, Integer, UnicodeText, DateTime, Boolean, ForeignKey, Table
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-
-from ..lib.providers.pd.pdmanager import PDManager
-from ..lib.config import config
-
-# pylint: disable=redefined-outer-name,missing-docstring
-
-log = logging.getLogger(__name__)  # pylint: disable=invalid-name
+log = logging.getLogger(__name__)
 logging.basicConfig(
-    format='%(asctime)s.%(msecs)-3d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
-    datefmt='%d-%m-%Y:%H:%M:%S',
-    level='INFO')
+    format="%(asctime)s.%(msecs)-3d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
+    datefmt="%d-%m-%Y:%H:%M:%S",
+    level="INFO",
+)
 
 
 def test_users_add(session_factory, fake_user_maker):
@@ -45,21 +37,74 @@ def test_to_json(fake_user_maker):
     pprint(fake_user_maker.make().to_json())
 
 
-# def test_from_json():
-#     event_loop = asyncio.new_event_loop()
-#     asyncio.set_event_loop(event_loop)
+@pytest.mark.skip("Test data needs to be prepared")
+def test_from_json():
+    event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
 
-#     user_id = 'someuser'
+    user_id = "someuser"
 
-#     async def run_test():
-#         user = await get_user_from_remote(user_id)
-#         print(json.dumps(user.to_json(), indent=4))
+    async def run_test():
+        user = await get_user_from_remote(user_id)
+        print(json.dumps(user.to_json(), indent=4))
 
-#     coro = asyncio.coroutine(run_test)
-#     event_loop.run_until_complete(coro())
-#     event_loop.close()
+    coro = asyncio.coroutine(run_test)
+    event_loop.run_until_complete(coro())
+    event_loop.close()
 
 
-def test_multiple_sessions(session_factory):
-    with session_factory.make_session() as session:
-        pass
+@pytest.mark.skip("Fix later")
+def test_get_tags():
+    event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
+
+    config.load_config(config.default_config_file())
+    pdm = PDManager(config.raw_data["api_clients"])
+
+    async def run_test():
+        user = await pdm.get_tags("id1", "id2")
+        print(json.dumps(user, indent=4))
+
+    coro = asyncio.coroutine(run_test)
+    event_loop.run_until_complete(coro())
+    event_loop.close()
+
+
+def test_multiple_sessions(session_factory, fake_user_maker):
+    event_loop = asyncio.get_event_loop()
+    with session_factory.make_session() as outer_session:
+        outer_session.add(fake_user_maker.make())
+        outer_session.commit()
+        for usr in outer_session.query(User).order_by(User.date_added.desc()).all():
+            print("  -  ", usr.first_name, usr.last_name)
+
+        def session1():
+            with session_factory.make_session() as session:
+                users = [fake_user_maker.make() for _ in range(10)]
+                for user in users:
+                    print(f"[coro1] Adding {user.first_name}")
+                    session.add(user)
+                session.commit()
+                return len(users)
+
+        def session2():
+            with session_factory.make_session() as session:
+                users = [fake_user_maker.make() for _ in range(10)]
+                for user in users:
+                    print(f"[coro2] Adding {user.first_name}")
+                    session.add(user)
+                session.commit()
+                return len(users)
+
+        async def go():
+            with concurrent.futures.ThreadPoolExecutor() as pool:
+                return await asyncio.wait(
+                    [
+                        event_loop.run_in_executor(pool, session1),
+                        event_loop.run_in_executor(pool, session2),
+                    ],
+                    loop=event_loop,
+                )
+
+        event_loop.run_until_complete(go())
+    print("Done")
